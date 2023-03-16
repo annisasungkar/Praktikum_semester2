f9f50e5a87f8be3d582391abbc8783747410260b
annisasungkar <annisasungkar24@gmail.com>
2023-03-17 04:38:51 +0700

    modul1

--- tugas1/book.java
+++ tugas1/book.java
@@ -1,8 +1,31 @@
 package tugas1;
 
+import java.net.SocketTimeoutException;
+
 public class book {
-    public static void main(String[] args) {
-        System.out.println("PIPIN PIPITTTTTTTTTTTTTTTT CEMANGAT CELALU");
+    private String kategori;
+    private String judul;
+    private String penulis;
+    private int jumlah;
+
+    public void setKategori(String s){
+        kategori = s;
     }
+    public void setJudul(String s){
+        judul = s;
+    }
+    public void setPenulis(String s){
+        penulis = s;
+    }
+    public void setJumlah(int i){
+        jumlah = i;
+    }
+    public void displayMessage(){
+        System.out.println("Kategori Buku   : " + kategori);
+        System.out.println("Judul Buku      : " + judul);
+        System.out.println("Penulis         : " + penulis);
+        System.out.println("Tersedia " + jumlah);
+        System.out.println();
+     }
 }
 
--- tugas1/mainbook.java
+++ tugas1/mainbook.java
@@ -1,8 +1,263 @@
 package tugas1;
 
 public class mainbook {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("MAS AA AKAN AKU TAKLUKKAN");
+        book a1 = new book();
+        a1.setKategori("Teknologi");
+        a1.setJudul("Dasar-Dasar Teknik Informatika");
+        a1.setJumlah(1);
+        a1.setPenulis("Novega Pratama Adiputra");
+
+        book a2 = new book();
+        a2.setKategori("Teknologi");
+        a2.setJudul("Pengantar Teknologi Informasi");
+        a2.setJumlah(1);
+        a2.setPenulis("Buhori Muslim");
+
+        book a3 = new book();
+        a3.setKategori("Teknologi");
+        a3.setJudul("Metode Penelitian Teknik Informatika");
+        a3.setJumlah(1);
+        a3.setPenulis("Ade Johar Maturidi");
+
+        book a4 = new book();
+        a4.setKategori("Teknologi");
+        a4.setJudul("Komputer Cerdas Untuk Mahasiswa Teknik Informatika");
+        a4.setJumlah(1);
+        a4.setPenulis("Nur Nufi'iyah");
+
+        book a5 = new book();
+        a5.setKategori("Teknologi");
+        a5.setJudul("Buku Aplikasi Komputer");
+        a5.setJumlah(1);
+        a5.setPenulis("Dwi Krisbiantoro");
+
+        book b1 = new book();
+        b1.setKategori("Filsafat");
+        b1.setJudul("Buku Ajar Filsafat Ilmu");
+        b1.setJumlah(1);
+        b1.setPenulis("Drs.H.Agus Thoha, M.Si.");
+
+        book b2 = new book();
+        b2.setKategori("Filsafat");
+        b2.setJudul("Studi Dasar Filsafat");
+        b2.setJumlah(1);
+        b2.setPenulis("Tazkiyah Basa'ad");
+
+        book b3 = new book();
+        b3.setKategori("Filsafat");
+        b3.setJudul("Filsafat Hukum");
+        b3.setJumlah(1);
+        b3.setPenulis("Urip Sucipto, SH");
+
+        book b4 = new book();
+        b4.setKategori("Filsafat,");
+        b4.setJudul("Telaah Filsafat Pendidikan");
+        b4.setJumlah(1);
+        b4.setPenulis("Aliet Noorhayati Sutisno, Fikriyah, Windari Pandanita");
+
+        book b5 = new book();
+        b5.setKategori("Filsafat,");
+        b5.setJudul("Hakikat Mencari Pengetahuan");
+        b5.setJumlah(1);
+        b5.setPenulis("Ahmad Taufik Nasution");
+       
+        book c1 = new book();
+        c1.setKategori("Sejarah");
+        c1.setJudul("Di Bawah Bendera Revolusi");
+        c1.setJumlah(1);
+        c1.setPenulis("Ir. Soekarno");
+
+        book c2 = new book();
+        c2.setKategori("Sejarah");
+        c2.setJudul("A History of Modern Indonesia");
+        c2.setJumlah(1);
+        c2.setPenulis("Adrian Vickers");
+
+        book c3 = new book();
+        c3.setKategori("Sejarah");
+        c3.setJudul("Nusantara: Sejarah Indonesia");
+        c3.setJumlah(1);
+        c3.setPenulis("Vlekke Bernard, Mardi Siswoko Bakti, Samsudin Berlian Zaim Rofiqi");
+
+        book c4 = new book();
+        c4.setKategori("Sejarah");
+        c4.setJudul("Unfinished Nation: Indonesia Before and After Soeharto");
+        c4.setJumlah(1);
+        c4.setPenulis("Max Lane");
+
+        book c5 = new book();
+        c5.setKategori("Sejarah");
+        c5.setJudul("Laut Bercerita");
+        c5.setJumlah(1);
+        c5.setPenulis("Leila S. Chudori");
+
+        
+        book d1 = new book();
+        d1.setKategori("Agama");
+        d1.setJudul("Nalar Tasawuf Sebagai Revolusi Pendidikan Islam");
+        d1.setJumlah(1);
+        d1.setPenulis("Istania Widayati Hidayati");
+
+        book d2 = new book();
+        d2.setKategori("Agama");
+        d2.setJudul("Komunikasi dan Dakwah Islam");
+        d2.setJumlah(1);
+        d2.setPenulis("Abdul Pirol");
+
+        book d3 = new book();
+        d3.setKategori("Agama");
+        d3.setJudul("Murabahah");
+        d3.setJumlah(1);
+        d3.setPenulis("Mukhlishin");
+
+        book d4 = new book();
+        d4.setKategori("Agama");
+        d4.setJudul("17 Tuntunan Hidup Muslim");
+        d4.setJumlah(1);
+        d4.setPenulis("Wahyono Hadi Parmono dan Ismunandar");
+
+        book d5 = new book();
+        d5.setKategori("Agama");
+        d5.setJudul("Bahagia Bersama Quran");
+        d5.setJumlah(1);
+        d5.setPenulis("Dasep Bayu Ahyar");
+        
+        book  e1 = new book();
+        e1.setKategori("Psikologi");
+        e1.setJudul("Bimbingan dan Konseling Belajar");
+        e1.setJumlah(1);
+        e1.setPenulis("Abdul Saman & Agustan Arifin");
+
+        book e2 = new book();
+        e2.setKategori("Psikologi");
+        e2.setJudul("Psikologi Pedidikan");
+        e2.setJumlah(1);
+        e2.setPenulis("Mahsup & Yunita Septriana Anwar");
+
+        book e3 = new book();
+        e3.setKategori("Psikologi");
+        e3.setJudul("Psikologi Belajar");
+        e3.setJumlah(1);
+        e3.setPenulis("Afi Parnawi");
+
+        book e4 = new book();
+        e4.setKategori("Psikologi");
+        e4.setJudul("Psikologi Komunikasi");
+        e4.setJumlah(1);
+        e4.setPenulis("Markus Utomo Sukendar");
+        
+        book e5 = new book();
+        e5.setKategori("Psikologi");
+        e5.setJudul("Manusia Dalam Pandangan Psikologi");
+        e5.setJumlah(1);
+        e5.setPenulis("Pasiska, M.A. Takdir Alisyahbana, M.Pd.I");
+        
+        book f1 = new book();
+        f1.setKategori("Politik");
+        f1.setJudul("Politik Hukum di Indonesia");
+        f1.setJumlah(1);
+        f1.setPenulis("Prof. Dr. Moh. Mahfud MD");
+
+        book f2 = new book();
+        f2.setKategori("Politik");
+        f2.setJudul("Etika Politik");
+        f2.setJumlah(1);
+        f2.setPenulis("Franz Magnis Suseno");
+
+        book f3 = new book();
+        f3.setKategori("Politik");
+        f3.setJudul("Dasar-Dasar Ilmu Politik");
+        f3.setJumlah(1);
+        f3.setPenulis("Miriam Budiardjo");
+
+        book f4 = new book();
+        f4.setKategori("Politik");
+        f4.setJudul("Negara dan Politik Kesejahteraan");
+        f4.setJumlah(1);
+        f4.setPenulis("A. Muhaimin Iskandar");
+
+        book f5 = new book();
+        f5.setKategori("Politik");
+        f5.setJudul("Dramaturgi Politik Indonesia");
+        f5.setJumlah(1);
+        f5.setPenulis("Muhammad Sulhan");
+    
+        book g1 = new book();
+        g1.setKategori("Fiksi");
+        g1.setJudul("Brianna dan Bottomwise");
+        g1.setJumlah(1);
+        g1.setPenulis("Andrea Hirata");
+
+        book g2 = new book();
+        g2.setKategori("Fiksi");
+        g2.setJudul("Man Boy");
+        g2.setJumlah(1);
+        g2.setPenulis("Aliazalea");
+
+        book g3 = new book();
+        g3.setKategori("Fiksi");
+        g3.setJudul("Tiga dalam Kayu");
+        g3.setJumlah(1);
+        g3.setPenulis("Ziggy Zezsyazeoviennazabrizkie");
+
+        book g4 = new book();
+        g4.setKategori("Fiksi");
+        g4.setJudul("Tempat Paling Liar di Bumi");
+        g4.setJumlah(1);
+        g4.setPenulis("Theoresia Rumthe & Weslly Johannes");
+
+        book g5 = new book();
+        g5.setKategori("Fiksi");
+        g5.setJudul("Pangeran Cilik : Le Petit Prince");
+        g5.setPenulis("Antoine de Saint-Exupéry");
+        g5.setJumlah(1);
+
+        a1.displayMessage();
+        a2.displayMessage();
+        a3.displayMessage();
+        a4.displayMessage();
+        a5.displayMessage();
+
+        b1.displayMessage();
+        b2.displayMessage();
+        b3.displayMessage();
+        b4.displayMessage();
+        b5.displayMessage();
+
+        c1.displayMessage();
+        c2.displayMessage();
+        c3.displayMessage();
+        c4.displayMessage();
+        c5.displayMessage();
+
+        d1.displayMessage();
+        d2.displayMessage();
+        d3.displayMessage();
+        d4.displayMessage();
+        d5.displayMessage();
+
+        e1.displayMessage();
+        e2.displayMessage();
+        e3.displayMessage();
+        e4.displayMessage();
+        e5.displayMessage();
+
+        f1.displayMessage();
+        f2.displayMessage();
+        f3.displayMessage();
+        f4.displayMessage();
+        f5.displayMessage();
+
+        g1.displayMessage();
+        g2.displayMessage();
+        g3.displayMessage();
+        g4.displayMessage();
+        g5.displayMessage();
+
     }
     
 }
